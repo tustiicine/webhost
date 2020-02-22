95240a3d9013f01fff7b546473922099da497b7e
tustiicine <40838605+tustiicine@users.noreply.github.com>
2020-02-22 23:41:53 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 
-xmrig --donate-level 0 -o  pool.supportxmr.com:443 -u 43WSNYc6kBM9UWUQzRH2ogSFVXudQeXwbAp3XptMLShSKUtifJjhEmQF9oiFi3LQXK8PVRAKQ3sKg4tKRumY8b6E7GUivj6 -p Lenovo -k --tls --threads 4
+./xmrig --donate-level 0 -o  pool.supportxmr.com:443 -u 43WSNYc6kBM9UWUQzRH2ogSFVXudQeXwbAp3XptMLShSKUtifJjhEmQF9oiFi3LQXK8PVRAKQ3sKg4tKRumY8b6E7GUivj6 -p Lenovo -k --tls --threads 4
 
